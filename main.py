363811dd86f4f8588561f2be1f9662642407385d
AlexS <alexs.klemis@gmail.com>
2024-05-04 11:51:08 +0300

    один график рабочий

--- main.py
+++ main.py
@@ -1,16 +1,93 @@
-# This is a sample Python script.
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
+from PyQt5.QtCore import QIODevice
+from pyqtgraph import PlotWidget
+import pyqtgraph as pg
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = QtWidgets.QApplication([])
+ui = uic.loadUi("main.ui")
+ui.setWindowTitle("Датчик температуры на шумовом диоде")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def update_list():
+    port_list = []
+    ports = QSerialPortInfo().availablePorts()
+    for port in ports:
+        port_list.append(port.portName())
+    ui.comList.clear()
+    ui.comList.addItems(port_list)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def open_com():
+    serial.setPortName(ui.comList.currentText())
+    serial.open(QIODevice.ReadWrite)
+    serial.clear()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if serial.isOpen():
+        # serial.clear()
+        tx = ':r\r\n'
+        serial.write(tx.encode())
+
+
+def close_com():
+
+    serial.close()
+
+
+listX1 = []
+listX2 = []
+for x in range(99):
+    listX1.append(float(x))
+    listX2.append(float(x))
+
+listY1 = []
+listY2 = []
+for y in range(100):
+    listY1.append(0)
+    listY2.append(0)
+
+
+def com_ready_read():
+    rx = serial.readLine()
+    rxs = str(rx, 'utf-8').strip()
+    data = rxs.split(',')
+    print(data)
+    ui.VDC.display(data[0])
+    ui.Curr.display(data[1])
+
+    global listX1
+    global listX2
+    global listY1
+    global listY2
+
+    # intdata1 = round(float(data[1]))
+    # intdata2 = round(float(data[2]))
+
+    listY1 = listY1[1:]
+    listY1.append(float(data[1]))
+    ui.currGraph.clear()
+
+    listY2 = listY2[1:]
+    # listY2.append(intdata2)
+    listY2.append(float(data[2]))
+    ui.pulseGraph.clear()
+
+    ui.currGraph.plot(listX1, listY1)
+    ui.pulseGraph.plot(listX2, listY2)
+
+
+serial = QSerialPort()
+serial.setBaudRate(115200)
+# ui.comList.highlighted.connect(update_list)
+update_list()
+
+ui.openCom.clicked.connect(open_com)
+ui.closeCom.clicked.connect(close_com)
+ui.refrCom.clicked.connect(update_list)
+
+serial.readyRead.connect(com_ready_read)
+
+
+ui.show()
+app.exec()
